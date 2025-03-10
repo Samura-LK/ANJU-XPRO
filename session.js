2166d7d10aa975703ea32e846209d93979b252e3
Samura-LK <kingdilisha4@gmail.com>
2025-03-10 07:05:04 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,8 +40,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
-SUDO: process.env.SUDO || ["94717775628","94758775628"],
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~2dcDDbTT#FHhdRbl2cwLNfiQgYEzm0UjfLm4WkPU1qMO8COt6Tqo",
+SUDO: process.env.SUDO || ["94771098429","0775700815"],
 PORT: process.env.PORT || "8000",
-FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n🌟 Premium Version | Advanced Features | Secure & Reliable 🌟\n© 2025 QUEEN ANJU XPRO. All rights reserved.",
+FOOTER: process.env.FOOTER || "> POWED BY KING CIMEMA\n> SRILANKAN NO1 MOVIE FAMILY.",
 };
